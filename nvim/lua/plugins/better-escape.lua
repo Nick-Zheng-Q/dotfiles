93bcf40e5999e6990236e9e98c6d95af019e627c
Nick-Zheng-Q <980756220@qq.com>
2025-05-23 10:07:07 +0800

    modified better escape configuration

--- nvim/lua/plugins/better-escape.lua
+++ nvim/lua/plugins/better-escape.lua
@@ -1,6 +1,22 @@
-return{-- lua with lazy.nvim 
+return { -- lua with lazy.nvim
 	{
-	  "max397574/better-escape.nvim",
-	  opts = {}
-	}
+		"max397574/better-escape.nvim",
+		opts = {
+			default_mappings = false,
+			mappings = {
+				-- i for insert, other modes are the first letter too
+				i = {
+					-- map kj to exit insert mode
+					k = {
+						j = "<Esc>",
+					},
+					-- map jk and jj  to exit insert mode
+					j = {
+						k = "<Esc>",
+						j = "<Esc>",
+					},
+				},
+			},
+		},
+	},
 }
